0331e5cbdd58440408656452d3f03751b38b7470
arhaidar <arhaidar@uci.edu>
2024-12-10 13:32:08 -0800

    added condition to only show recipes that have pictures, otherwise dont' display it

--- scripts/results.js
+++ scripts/results.js
@@ -6,8 +6,13 @@ document.addEventListener("DOMContentLoaded", () => {
         resultsContainer.innerHTML = "<p>No recipes found. Try refining your search!</p>";
         return;
     }
-
-    resultsContainer.innerHTML = results.map(recipe => `
+    // only get the recipes that have valid image -> dont shw just alt text in case of no image
+    const filteredResults = results.filter(recipe => recipe.image && recipe.image.trim() !== "");
+    if(filteredResults.length === 0){ // no recipes with valid images
+        resultsContainer.innerHTML = "<p>No recipes with valid images found. Try refining your search!</p>";
+        return;
+    }
+    resultsContainer.innerHTML = filteredResults.map(recipe => `
         <div class="recipe-card">
             <img src="${recipe.image}" alt="${recipe.title}">
             <h2>${recipe.title}</h2>
